07df12d0c3244eeec04ab469d13e03eb5ac45088
Marcus <marcusfonseca15@gmail.com>
2025-07-01 17:50:05 -0300

    first commit

---
+++ api/src/test/java/sync/fit/api/ApiAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package sync.fit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
