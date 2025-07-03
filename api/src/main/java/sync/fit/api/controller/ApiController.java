ec010bf7703cd8e7227ebe3a0694e537efc14c04
Mateus Santos <mateussilvanzg@gmail.com>
2025-07-03 19:13:38 -0300

    fix: configure dependencies

---
+++ api/src/main/java/sync/fit/api/controller/ApiController.java
@@ -0,0 +1,13 @@
+package sync.fit.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+    @GetMapping
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello World. My name Mateus! =)");
+    }
+}
