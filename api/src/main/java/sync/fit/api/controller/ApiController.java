728a764509bd5bf60bc7a1d5a9c60f4540da8d1b
Mateus Santos <mateussilvanzg@gmail.com>
2025-07-06 15:11:47 -0300

    feat: new methods

--- api/src/main/java/sync/fit/api/controller/ApiController.java
+++ api/src/main/java/sync/fit/api/controller/ApiController.java
@@ -1,13 +1,39 @@
 package sync.fit.api.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ApiController {
-    @GetMapping
-    public ResponseEntity<String> helloWorld() {
-        return ResponseEntity.ok("Hello World. My name Mateus! =)");
+
+    private List<String> clientes = new ArrayList<>();
+
+    private ObjectMapper objectMapper;
+
+    public ApiController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        
+    }
+
+    @GetMapping(path = "/clientes")
+    public ResponseEntity<String> listClientes() throws JsonProcessingException {
+        return ResponseEntity.ok(objectMapper.writeValueAsString(clientes));
+    }
+
+    @PostMapping(path = "/clientes")
+    public ResponseEntity<String> addClient(@RequestBody String cliente) {
+        clientes.add(cliente);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(path = "/clientes")
+    public ResponseEntity<Void> clearClients() {
+        clientes = new ArrayList<>();
+        return ResponseEntity.ok().build();
     }
 }
