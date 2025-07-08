cd7ed707606a0ec6edf781a091b601cff136e34a
Marcus <marcusfonseca15@gmail.com>
2025-07-07 21:17:20 -0300

    .

--- api/src/main/java/sync/fit/api/dto/response/ClienteResponseDTO.java
+++ api/src/main/java/sync/fit/api/dto/response/ClienteResponseDTO.java
@@ -1,4 +1,61 @@
 package sync.fit.api.dto.response;
 
 public class ClienteResponseDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String planoTipo;
+    private String administradorNome;
+
+    public ClienteResponseDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getPlanoTipo() {
+        return planoTipo;
+    }
+
+    public void setPlanoTipo(String planoTipo) {
+        this.planoTipo = planoTipo;
+    }
+
+    public String getAdministradorNome() {
+        return administradorNome;
+    }
+
+    public void setAdministradorNome(String administradorNome) {
+        this.administradorNome = administradorNome;
+    }
 }
--- api/src/main/java/sync/fit/api/dto/response/PlanoResponseDTO.java
+++ api/src/main/java/sync/fit/api/dto/response/PlanoResponseDTO.java
@@ -1,4 +1,43 @@
 package sync.fit.api.dto.response;
 
 public class PlanoResponseDTO {
+    private Long id;
+    private String tipo;
+    private double valor;
+    private int duracaoMeses;
+
+    public PlanoResponseDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public int getDuracaoMeses() {
+        return duracaoMeses;
+    }
+
+    public void setDuracaoMeses(int duracaoMeses) {
+        this.duracaoMeses = duracaoMeses;
+    }
 }
